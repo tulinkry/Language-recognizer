9ab13c5262bfe36325592551024b625bec1e316d
katrinp <atrit@seznam.cz>
2014-09-17 21:38:44 +0200

    need improve command line arguments

--- langRecognizer.py
+++ langRecognizer.py
@@ -1,4 +1,9 @@
-import ngrams, operator, collections, langVector
+import ngrams, operator, collections, langVector, sys
+
+#HOW TO IMPROVE: use collections.OrderedDict (pro záznam skóre)
+#TODO: shoda u určení jazyků
+#TODO: váhy! - viz HOW TO IMROVE
+
 
 #count a score for sentence from vector of a language ~ probability for ngrams in the language (suma of logarithms)
 def count_ngram_score(sentence, vector, n):
@@ -16,9 +21,8 @@ def recognize_language(sentence, vectors, n):
 		scores.append({})
 		for language in vectors.keys():
 			scores[i][language] = count_ngram_score(sentence, vectors[language], i+1)
-	print(scores)	
+
 	#result for uni/bi/trigrams - 0 ~ uni etc.
-	#vyřešit - shoda u určení jazyků, váhy
 	result = []
 	for i in range(0,n):
 		#find the language with largest score for i-gram
@@ -30,17 +34,11 @@ def recognize_language(sentence, vectors, n):
 	return language, probability
 
 
-
-
-#langVector.add_language_vector("czech", "cs/train-cs-100.txt", "language_vector.p")
-#langVector.add_language_vector("norwegian", "no/train-no.txt", "language_vector.p")
-#langVector.add_language_vector("english", "en/train-en.txt", "language_vector.p")
-
 vectors = langVector.load_vector("language_vector.p")
-#czech = vector_of_language("cs/train-cs-10.txt")
-#english = vector_of_language("en/train-en.txt")
-#norwegian = vector_of_language("no/train-no.txt")
-#vectors = {"czech": czech}
-print(recognize_language("ahoj, jak se máš", vectors, 3))
-print(recognize_language("I like måte, hyggilig", vectors, 3))
+
+if len(sys.argv) < 2:
+	sentence = input("Your sentence: ")
+else:
+	sentence = sys.argv[1]
+print(recognize_language(sentence, vectors, 3))
 
