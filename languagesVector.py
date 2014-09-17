ef45fc398726213af34ea30c1809407f9f4171d1
katrinp <atrit@seznam.cz>
2014-09-17 15:32:06 +0200

    first version of recognizing, dont checked if it works...

--- languagesVector.py
+++ languagesVector.py
@@ -1,4 +1,7 @@
-import ngrams
+import (ngrams,
+		operator,
+		collections,
+)		
 
 #make a propbability list for one language
 #source_file - file with plain text
@@ -11,24 +14,37 @@ def vector_of_language(source_file):
 	trigram_probability = ngrams.probability_of_trigram(ngrams.count_ngrams(text, 3))
 	return [unigram_probability, bigram_probability, trigram_probability]
 
-
-def count_unigram_score(sentence, vector):
-	score = 0
-	for letter in list(sentence):
-		if letter in vector[0]:
-			score += vector[0][letter]
-	return score
-
 #count a score for sentence from vector of a language ~ probability for ngrams in the language (suma of logarithms)
 def count_ngram_score(sentence, vector, n):
 	score = 0
 	ngrams_array = ngrams.make_ngrams(sentence, n)
-	for ngram in ngrams_array:
-		
+	for ngram in ngrams_array:	
 		if ngram in vector[n-1]:
 			print(ngram)
 			score += vector[n-1][ngram]
 	return score
+
+def add_language_vector(output_file, train_file):
+	
+def recognize_language(sentence, vectors, n)
+	scores = []
+	for i in range(0,n-1):
+		for language in vectors:
+		scores[i] = {}
+		scores[i][language] = count_ngram_score(sentence, vectors[language], i)
+	
+	#result for uni/bi/trigrams - 0 ~ uni etc.
+	#vyřešit - shoda u určení jazyků, váhy
+	result = []
+	for i in range(0,n-1):
+		#find the language with largest score for i-gram
+		result[i] = max(scores[i].iteritems(), key=operator.itemgetter(1))[0]
+	counted_result = collections.Counter(result)
+	#find the languge which is mostly appeared - dořešit váhy!!!
+	language = max(counted_result[i].iteritems(), key=operator.tremgetter(1))[0]
+	probability = counted_result{language} / sum(counted_result.values())
+	return language, probability
+
 
 czech = vector_of_language("train_cs_500lines.txt")
 english = vector_of_language("en/train-en.txt")
